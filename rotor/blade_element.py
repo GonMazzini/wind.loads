ca55fdfea6bf3d816e4d565bccf832ecfcb0ad80
GonMazzini <gmazzini@itba.edu.ar>
2021-01-19 20:37:20 -0300

    initial docstring for rotor

--- rotor/blade_element.py
+++ rotor/blade_element.py
@@ -1,4 +1,24 @@
 """Standard library imports"""
 import numpy as np  # 1.19.4
 import pandas as pd  # 1.2.0
-import scipy as sp  #
+import scipy as sp  # 1.6.0
+
+print(f'numpy version {np.__version__} , \t pandas version {pd.__version__} , \t scipy version {sp.__version__}')
+
+
+class Rotor():
+    """
+    Class to represent the wind turbine rotor.
+
+    Attributes
+    ----------
+    blades_num : int
+        number of blades of the rotor.
+    radio:
+        radio of the rotor.
+
+    Methods
+    -------
+    lift_drag_coef
+
+    """
